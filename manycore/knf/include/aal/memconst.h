2cea96e7572f715dd2285bbc45a7d2491a71d64e
Taku Shimosawa <shimosawa@is.s.u-tokyo.ac.jp>
2012-02-22 18:32:53 +0900

    DMA Align const added in KNF

--- manycore/knf/include/aal/memconst.h
+++ manycore/knf/include/aal/memconst.h
@@ -6,6 +6,8 @@
 #define AAL_KMSG_SIZE      8192
 #define AAL_KMSG_ALIGN     __attribute__((aligned(4096)))
 
+#define AAL_DMA_ALIGN      __attribute__((aligned(64)))
+
 #define AAL_PTA_REMOTE     PTATTR_UNCACHABLE
 
 #endif
