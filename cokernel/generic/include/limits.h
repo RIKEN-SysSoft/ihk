87413d237975579f89c2e89fe06b5cdd9cfdc6cb
Yuji Saeki <ysaeki@victoria.crl.hitachi.co.jp>
2012-12-17 15:52:30 +0900

    remove old directories

--- cokernel/generic/include/limits.h
+++
@@ -1,7 +0,0 @@
-#ifndef __HEADER_LIMITS
-#define __HEADER_LIMITS
-
-#define INT_MAX 0x7fffffff
-#define INT_MIN -0x80000000
-
-#endif
--- cokernel/generic/include/types.h
+++
@@ -1,7 +0,0 @@
-#ifndef TYPES_H
-#define TYPES_H
-
-#include <aal/types.h>
-
-#endif
-
