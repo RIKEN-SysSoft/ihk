8efa1c30fb5fdead42dbe92bb38ea46bc435f14a
Masamichi Takagi <masamichi.takagi@riken.jp>
2016-08-17 17:40:00 +0900

    Specify facility used by mcklogd via option
    1. You can specify facility through -f option. Example:
          mcklogd -f LOG_LOCAL6
    2. Source code is moved to linux/usr/
    3. Remove mcklogd.ini

--- linux/user/mcklogd.c
+++ linux/user/mcklogd.c
@@ -12,76 +12,36 @@
 #define FLAG_KILL					2
 
 #define KPRINTF_LOCAL_BUF_LEN 1024
-
 int main(int argc, char **argv)
 {
 	int opt;
 	int flag = 0;
 	int nochdir = 1;
 	int noclose = 0;
-	int count = 0;
 	int interval = 1;
-	int buffer_wait = 0;
 	int facility = LOG_LOCAL6;
 	int i;
-	int len;
 	char pid_file_path[] = "./mcklogd.pid";
 	char buf[512];
 	char command[512];
-	char inifile[512];
-	char item[256];
-	char value[256];
 	char *envptr;
-	char *delimiter;
 	FILE *pid_file;
 	FILE *fp;
 	pid_t pid;
+
 	struct {
 		char name[12];
 		int code;
 	} facility_list[8] = {
-		"LOG_LOCAL0", LOG_LOCAL0,
-		"LOG_LOCAL1", LOG_LOCAL1,
-		"LOG_LOCAL2", LOG_LOCAL2,
-		"LOG_LOCAL3", LOG_LOCAL3,
-		"LOG_LOCAL4", LOG_LOCAL4,
-		"LOG_LOCAL5", LOG_LOCAL5,
-		"LOG_LOCAL6", LOG_LOCAL6,
-		"LOG_LOCAL7", LOG_LOCAL7
+		{"LOG_LOCAL0", LOG_LOCAL0},
+		{"LOG_LOCAL1", LOG_LOCAL1},
+		{"LOG_LOCAL2", LOG_LOCAL2},
+		{"LOG_LOCAL3", LOG_LOCAL3},
+		{"LOG_LOCAL4", LOG_LOCAL4},
+		{"LOG_LOCAL5", LOG_LOCAL5},
+		{"LOG_LOCAL6", LOG_LOCAL6},
+		{"LOG_LOCAL7", LOG_LOCAL7}
 	};
-
-	/**
-	 * mcklogd[PID]: log message
-	 * default facility: LOG_LOCAL6 (/var/log/local6)
-	 */
-	strcpy(inifile, "./mcklogd.ini");
-	fp = fopen(inifile, "r");
-	if (fp != NULL) {
-		while(fgets(buf, sizeof(buf), fp) != NULL) {
-			if (buf[0] == '#') continue;
-			len = strlen(buf);
-			if (len > 0) {
-				if (buf[len-1] == '\n') {
-					buf[len-1] = '\0';
-				}
-			}
-			delimiter = strstr(buf, "=");
-			if (delimiter == NULL) continue;
-			*delimiter = '\0';
-			sscanf(buf, "%s", item);
-			sscanf(delimiter+1, "%s", value);
-			if (strcmp(item, "FACILITY") == 0) {
-				for (i = 0; i < 8; i++) {
-					if (strcmp(value, facility_list[i].name) == 0) {
-						facility = facility_list[i].code;
-						break;
-					}
-				}
-			}
-		}
-	}
-	openlog("mcklogd", LOG_PID, facility);
-
 	const struct option longopt[] = {
 		{"help", no_argument, NULL, '?'},
 		{"daemonize", no_argument, NULL, 'd'},
@@ -92,7 +52,7 @@ int main(int argc, char **argv)
 
 	flag = FLAG_DAEMONIZE;
 	
-	while ((opt = getopt_long(argc, argv, "dki:", longopt, NULL)) != -1) {
+	while ((opt = getopt_long(argc, argv, "dki:f:", longopt, NULL)) != -1) {
 		switch (opt) {
 		case 'i':
 			interval = atoi(optarg);
@@ -104,10 +64,26 @@ int main(int argc, char **argv)
 		case 'k':
 			flag = FLAG_KILL;
 			break;
+		case 'f':
+			/**
+			 * mcklogd[PID]: log message
+			 * default facility: LOG_LOCAL6 (/var/log/local6)
+			 */
+			for (i = 0; i < 8; i++) {
+				if (strcmp(optarg, facility_list[i].name) == 0) {
+					facility = facility_list[i].code;
+					goto found;
+				}
+			}
+			fprintf(stderr, "Facility not found\n");
+			return -1;
+		found:;
+			break;
 		default:
 			break;
 		}
 	}
+	openlog("mcklogd", LOG_PID, facility);
 
 	if (flag == FLAG_KILL) {
 		pid_file = fopen(pid_file_path, "r");
