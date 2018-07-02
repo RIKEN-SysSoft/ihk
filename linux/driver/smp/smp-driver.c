a98a994f7c2e6855e4fd025ebf723e4e219998f4
Dominique Martinet <asmadeus@codewreck.org>
2018-07-02 15:10:02 +0900

    Static analysis fixes
    Change-Id: I3683917330f500e54f5b7497b256113395336900

--- linux/driver/smp/smp-driver.c
+++ linux/driver/smp/smp-driver.c
@@ -3217,7 +3217,8 @@ static int smp_ihk_reserve_cpu(ihk_device_t ihk_dev, unsigned long arg)
 #endif
 			printk("%s: invalid CPU requested: %d\n",
 					__FUNCTION__, cpu);
-			return -EINVAL;
+			ret = -EINVAL;
+			goto out;
 		}
 	}
 
@@ -3563,7 +3564,11 @@ static int smp_ihk_reserve_mem(ihk_device_t ihk_dev, unsigned long arg)
 	/* Do the reservation */
 	mem_token = strsep(&mem_string, ",");
 	while (mem_token) {
-		smp_ihk_parse_mem(mem_token, &mem_size, &numa_id);
+		ret = smp_ihk_parse_mem(mem_token, &mem_size, &numa_id);
+		if (ret != 0) {
+			printk("IHK-SMP: reserve_mem: error: parsing memory string\n");
+			break;
+		}
 		ret = __ihk_smp_reserve_mem(mem_size, numa_id);
 		if (ret != 0) {
 			printk("IHK-SMP: reserve_mem: error: reserving memory\n");
