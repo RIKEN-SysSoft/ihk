b61afa2fec260c03d7ae41c749f5384a69b34da7
Shiratori, Takehiro <fj0300es@aa.jp.fujitsu.com>
2018-07-27 18:26:14 +0900

    ihk_os_create_pseudofs, ihk_os_create_pseudofs: Return -ECHILD when children are wait()-ed by others
    Refs: #1106
    Change-Id: If701b8c510b71599926322a4a9fc670877a43873
    
    original commit-id: f6761fdc35425bc977c56d1fdb6792bda3c591cd

--- test/ihklib/util.h
+++ test/ihklib/util.h
@@ -27,18 +27,20 @@
 	}				\
 } while (0)
 
-#define _OKNG(verb, cond, fmt, args...) do {                     \
-	if (cond) {                                              \
-		if (verb)                                        \
-			printf("[OK] " fmt, ##args);        \
-	} else {                                                 \
-		printf("[NG] " fmt ": %d", ##args, ret);       \
-		goto fn_fail;                                    \
-	}                                                        \
+#define _OKNG(verb, jump, cond, fmt, args...) do {	\
+	if (cond) {					\
+		if (verb)				\
+			printf("[OK] " fmt, ##args);	\
+	} else {					\
+		printf("[NG] " fmt, ##args);		\
+		if (jump)				\
+			goto fn_fail;			\
+	}						\
 } while (0)
 
-#define OKNG(args...) _OKNG(1, ##args)
-#define NG(args...) _OKNG(0, ##args)
+#define OKNG(args...) _OKNG(1, 1, ##args)
+#define NG(args...) _OKNG(0, 1, ##args)
+#define OKNGNOJUMP(args...) _OKNG(1, 0, ##args)
 
 #endif
 
