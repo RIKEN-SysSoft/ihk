RIKEN-SysSoft/ihk

commit 3af03b967768c66900c6a904f6958806d0a7414f

smp request_irq: handle refcount better
Linux since 3.1.0 (b6873807a714 ("irq: Track the owner of irq
descriptor")) will take a module reference on request_irq and put it on
free_irq, but we want to free_irq on module exit, which cannot be called
if there are extra refs.

Additionally, since newer version of linux, the refcount in module exit
is -1 and try_module_get there will not do anything, making the free_irq
warn needlessly that it cannot put the module.

Instead of letting the irq manage the owner module, immediately put the
ref and unset the irq owner immediately after obtaining it - module_put
is smart enough to not do anything with a NULL module.

Change-Id: Ic9183e1220b93c058e312ad4dade014e472841ae

linux/driver/smp/arch/arm64/smp-arch-driver.c
@@ -1123,6 +1123,13 @@ static int ihk_smp_reserve_irq(void)
 		pr_info("IHK-SMP: IRQ vector %d: request_irq failed\n", virq);
 		return -EFAULT;
 	}
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
+	/* drop the module ref from request_irq, and pretend we do not
+	 * own the irq anymore so free_irq will not bug the refcount
+	 */
+	module_put(THIS_MODULE);
+	desc->owner = NULL;
+#endif
 
 	ihk_smp_irq.irq = virq;
 	ihk_smp_irq.hwirq = (u32)(irq_desc_get_irq_data(desc)->hwirq);
@@ -1303,12 +1310,6 @@ retry_trampoline:
 	return error;
 
 error_free_irq:
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 3, 0)
-	if (this_module_put) {
-		try_module_get(THIS_MODULE);
-	}
-#endif
-
 	free_irq(ihk_smp_irq.irq, NULL);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,4,0)
@@ -1677,11 +1678,6 @@ int ihk_smp_reset_cpu(int hw_id)
 
 void smp_ihk_arch_exit(void)
 {
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 3, 0)
-	if (this_module_put) {
-		try_module_get(THIS_MODULE);
-	}
-#endif
 	free_irq(ihk_smp_irq.irq, NULL);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,4,0)

linux/driver/smp/arch/x86_64/smp-arch-driver.c
@@ -1187,6 +1187,13 @@ retry_trampoline:
 			printk(KERN_INFO "IHK-SMP: IRQ vector %d: request_irq failed\n", vector);
 			continue;
 		}
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
+		/* drop the module ref from request_irq, and pretend we do not
+		 * own the irq anymore so free_irq will not bug the refcount
+		 */
+		module_put(THIS_MODULE);
+		desc->owner = NULL;
+#endif
 
 		/* Pretend a real external interrupt */
 		for (i = 0; i < nr_cpu_ids; i++) {
@@ -1223,18 +1230,12 @@ retry_trampoline:
 	error = collect_topology();
 	if (error) {
 		printk(KERN_ERR "IHK-SMP: error: collecting topology information failed\n");
-		free_irq(ihk_smp_irq, NULL);
 		goto error_free_irq;
 	}
 
 	return error;
 
 error_free_irq:
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 3, 0)
-	if (this_module_put) {
-		try_module_get(THIS_MODULE);
-	}
-#endif
 
 	free_irq(ihk_smp_irq, NULL);
 
@@ -1613,11 +1614,6 @@ void smp_ihk_arch_exit(void)
 	}
 #endif
 
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(4, 3, 0)
-	if (this_module_put) {
-		try_module_get(THIS_MODULE);
-	}
-#endif
 	free_irq(ihk_smp_irq, NULL);
 
 #ifdef CONFIG_SPARSE_IRQ

linux/driver/smp/smp-driver.c
@@ -82,8 +82,6 @@ unsigned long trampoline_phys;
 
 unsigned long ident_page_table;
 
-int this_module_put = 0;
-
 static struct list_head ihk_mem_free_chunks;
 struct list_head ihk_mem_used_chunks;
 
@@ -4368,19 +4366,6 @@ static int __init smp_module_init(void)
 	}
 
 	builtin_data.ihk_dev = ihkd;
-
-#if ((LINUX_VERSION_CODE >= KERNEL_VERSION(3,0,0)) && \
-		(LINUX_VERSION_CODE <= KERNEL_VERSION(4,3,5)))
-	/* NOTE: this is nasty, but we need to decrease the refcount because
-	 * after Linux 3.0 request_irq holds an extra reference to the module. 
-	 * This causes rmmod to fail and report the module is in use when one
-	 * tries to unload it. To overcome this, we drop one ref here and get
-	 * an extra one before free_irq in the module's exit code */
-	if (module_refcount(THIS_MODULE) == 2) {
-		module_put(THIS_MODULE);
-		this_module_put = 1;
-	}
-#endif
 
 	return 0;
 }

linux/driver/smp/smp-driver.h
@@ -141,8 +141,6 @@ extern unsigned long trampoline_phys;
 
 extern unsigned long ident_page_table;
 
-extern int this_module_put;
-
 void *ihk_smp_map_virtual(unsigned long phys, unsigned long size);
 void ihk_smp_unmap_virtual(void *virt);
 int ihk_smp_set_nmi_mode(ihk_os_t ihk_os, void *priv, int mode);

commit 0ba60615c179b7f0c0cc7dcb6552ea589c0c107a

smp request_irq: fix refcount handling's fix
request_irq succeeding apparently does not always mean the irq
will be owned by the module.
Check desc->owner before doing module_put.

This fixes the module refcount going negative, making the module
effectively impossible to unload

Fixes: 3af03b967768 ("smp request_irq: handle refcount better")
Change-Id: Ib32d69cd3ef54afde19a558b4eae183308d658c5

linux/driver/smp/arch/arm64/smp-arch-driver.c
@@ -1126,9 +1126,13 @@ static int ihk_smp_reserve_irq(void)
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
 	/* drop the module ref from request_irq, and pretend we do not
 	 * own the irq anymore so free_irq will not bug the refcount
+	 * Only do this if we own the vector, this apparently is not
+	 * always true ?!
 	 */
-	module_put(THIS_MODULE);
-	desc->owner = NULL;
+	if (desc->owner == THIS_MODULE) {
+		module_put(THIS_MODULE);
+		desc->owner = NULL;
+	}
 #endif
 
 	ihk_smp_irq.irq = virq;

linux/driver/smp/arch/x86_64/smp-arch-driver.c
@@ -1190,9 +1190,13 @@ retry_trampoline:
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 1, 0)
 		/* drop the module ref from request_irq, and pretend we do not
 		 * own the irq anymore so free_irq will not bug the refcount
+		 * Only do this if we own the vector, this apparently is not
+		 * always true ?!
 		 */
-		module_put(THIS_MODULE);
-		desc->owner = NULL;
+		if (desc->owner == THIS_MODULE) {
+			module_put(THIS_MODULE);
+			desc->owner = NULL;
+		}
 #endif
 
 		/* Pretend a real external interrupt */