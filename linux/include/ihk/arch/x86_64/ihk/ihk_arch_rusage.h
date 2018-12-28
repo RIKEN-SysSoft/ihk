052f50158b9b446e376e3792af2ec79a4d51b442
Dominique Martinet <asmadeus@codewreck.org>
2018-12-28 17:25:14 +0900

    arm64/rusage: fix ihk_pgsizes type
    1ULL << 42 is too big to fit into an int
    
    Change-Id: I97b0b66dbcf5b5bc5b6dca64ce5fb513b049b848

--- linux/include/ihk/arch/x86_64/ihk/ihk_arch_rusage.h
+++ linux/include/ihk/arch/x86_64/ihk/ihk_arch_rusage.h
@@ -3,6 +3,6 @@
 
 #define IHK_NUM_PAGESIZES 3
 
-int ihk_pgsizes[] = { 1ULL<<12, 1ULL<<21, 1ULL<<30 };
+const long ihk_pgsizes[] = { 1ULL<<12, 1ULL<<21, 1ULL<<30 };
 
 #endif /* !defined(ARCH_RUSAGE_H_INCLUDED) */
