2bd509bcc857a8bb0df6f82f2ac0cb53a1d57d58
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-14 10:03:28 +0000

    smp_ihk_os_shutdown: Change status to BUILTIN_OS_STATUS_INITIAL when done
    Change-Id: I00be7f2ab8cd05a6c24b8adff83bf9a0e86565a4

--- linux/driver/smp/smp-driver.c
+++ linux/driver/smp/smp-driver.c
@@ -1167,6 +1167,8 @@ static int smp_ihk_os_shutdown(ihk_os_t ihk_os, void *priv, int flag)
 		free_pages((unsigned long)os->param, os->param_pages_order);
 	}
 
+	set_os_status(os, BUILTIN_OS_STATUS_INITIAL);
+
 	//kfree(os); /* done in destroy */
 
 	return ret;
