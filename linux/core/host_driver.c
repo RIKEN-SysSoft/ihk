0bb19405787b74d9bcc9868e7c77d9e6e9629544
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-02-17 12:24:20 +0000

    __ihk_device_destroy_os: Fix error handling
    Change-Id: I7d8e164e3d157ac171534f3c08f01fd487dcb1bd

--- linux/core/host_driver.c
+++ linux/core/host_driver.c
@@ -1667,7 +1667,8 @@ static int __ihk_device_destroy_os(struct ihk_host_linux_device_data *data,
 	}
 	
 	if (atomic_read(&os->refcount) > 0) {
-		dkprintf("%s: refcount != 0\n", __FUNCTION__);
+		pr_err("%s: error: refcount != 0 (%d)\n",
+		       __func__, atomic_read(&os->refcount));
 		return -EBUSY;
 	}
 
@@ -1675,11 +1676,13 @@ static int __ihk_device_destroy_os(struct ihk_host_linux_device_data *data,
 
 	if (data->ops->destroy_os) {
 		ret = data->ops->destroy_os(data, data->priv, os, os->priv);
+		if (ret) {
+			pr_err("%s: error: destroy_os: ret: %d\n",
+			       __func__, ret);
+			return -EINVAL;
+		}
 	}
 
-	if (ret != 0) {
-		return -EINVAL;
-	}
 
 	while (!list_empty(&os->event_list)) {
 		struct ihk_event *ep;
