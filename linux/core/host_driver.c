4331fda688460248187366155e4e780cf5fb1c05
Masamichi Takagi <masamichi.takagi@riken.jp>
2020-03-31 00:11:20 -0400

    workaround: Allow non-privileged to perform IHK_OS_REGISTER_EVENT
    Change-Id: I949c5d3b57d8378fe0c37c83ec127221deba6600

--- linux/core/host_driver.c
+++ linux/core/host_driver.c
@@ -1059,6 +1059,7 @@ static int __ihk_os_ioctl_perm(unsigned int request)
 	case IHK_OS_GET_USAGE:
 	case IHK_OS_GET_CPU_USAGE:
 	case IHK_OS_GET_NUM_CPUS:
+	case IHK_OS_REGISTER_EVENT:
 		break;
 	default:
 		if (request >= IHK_OS_DEBUG_START && 
