RIKEN-SysSoft/ihk

commit b1f7b4f18d02df6be84dc2bf3ca30e9ca41d540f

spec: Add ihk_host_driver.h to mckernel-devel
Change-Id: Ic49c5967ab91b824eb753c4053f209323ea7a104

linux/core/CMakeLists.txt
@@ -13,3 +13,6 @@ kmod(ihk
 	INSTALL_DEST
 		${KMODDIR}
 )
+
+install(FILES "../include/ihk/ihk_host_driver.h"
+	DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/ihk")

commit dd52fbbb9f364cce4ff827a729daafa4476f6801

cmake: Install headers and symbol table for driver development
Followings are installed in addition to ihk_host_driver.h
archdefs.h
status.h
ihk_monitor.h
ihk_debug.h
ihk/linux/core/Module.symvers

Fixes: b1f7b4f "spec: Add ihk_host_driver.h to mckernel-devel"
Change-Id: I84d7b2b73351c4d646d17589b1ed91fc8dc9644c

linux/core/CMakeLists.txt
@@ -14,5 +14,18 @@ kmod(ihk
 		${KMODDIR}
 )
 
-install(FILES "../include/ihk/ihk_host_driver.h"
-	DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/ihk")
+install(FILES ../include/ihk/arch/${ARCH}/ihk/archdefs.h
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/ihk")
+
+foreach(target IN ITEMS
+    status.h
+    ihk_monitor.h
+    ihk_debug.h
+    ihk_host_driver.h
+    )
+  install(FILES ../include/ihk/${target}
+    DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}/ihk")
+endforeach()
+
+install(FILES ${CMAKE_CURRENT_BINARY_DIR}/Module.symvers
+  DESTINATION "${KMODDIR}/ihk/linux/core")

linux/include/ihk/ihk_host_driver.h
@@ -10,6 +10,7 @@
 #ifndef __HEADER_IHK_HOST_DRIVER_H
 #define __HEADER_IHK_HOST_DRIVER_H
 
+#include <linux/list.h>
 #include <ihk/archdefs.h>
 #include <ihk/status.h>
 #include <ihk/ihk_monitor.h>