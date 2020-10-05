d0436a7c6298bed699800bb8d97b3b724b62c690
tinhnt86 <tinhnt86@gmail.com>
2020-10-05 11:43:54 +0700

    ihklib.c: ihk_get_os_instances: check invalid indices
    Change-Id: Ibdb1f51fb913529b28a663eac4c85367de944ec3

--- linux/user/ihklib.c
+++ linux/user/ihklib.c
@@ -1673,6 +1673,11 @@ int ihk_get_os_instances(int index, int *indices, int _num_os_instances)
 		goto out;
 	}
 
+	if (!indices) {
+		ret = -EFAULT;
+		goto out;
+	}
+
 	dir = opendir(PATH_DEV);
 	if (dir == NULL) {
 		ret = -errno;
