80bf38ea9f11be5695ad966b5f63fdfc7db88671
Masamichi Takagi <masamichi.takagi@riken.jp>
2021-02-12 12:35:41 +0900

    increase IHK_KMSG_SIZE to 8MiB
    Change-Id: Ibf3d55de58f3b675c3cfa843bb936f0683378f54

--- linux/include/ihk/ihk_debug.h
+++ linux/include/ihk/ihk_debug.h
@@ -8,7 +8,7 @@
 #ifndef IHK_DEBUG_H_INCLUDED
 #define IHK_DEBUG_H_INCLUDED
 
-#define IHK_KMSG_SIZE            8192
+#define IHK_KMSG_SIZE            (8 << 20)
 #define IHK_KMSG_HIGH_WATER_MARK (IHK_KMSG_SIZE / 2)
 #define IHK_KMSG_NOTIFY_DELAY    400 /* Unit is us, 400 us would avoid overloading fwrite of ihkmond */
 
